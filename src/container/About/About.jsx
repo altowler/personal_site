bb7d3fb330060c9ebab5f3c41d7a1e6ea2bfb860
Austin Towler <austin.towler@gmail.com>
2022-07-26 20:29:01 -0400

    initial commit

---
+++ src/container/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './About.scss';
+
+const About = () => {
+  return (
+    <div>This is my about section, this will tell you about me</div>
+  )
+}
+
+export default About
